be28c58a8c75c5c7a8ac6f8ae78c2c08835c47b8
ohideLawrence <96047870+ohideLawrence@users.noreply.github.com>
2021-12-14 11:34:56 +0800

    updated reply

--- src/org/test/HelloWorld.java
+++ src/org/test/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("how are you?");
-		System.out.println("hehe?");
+		System.out.println("hehe");
 		
 	}
 
